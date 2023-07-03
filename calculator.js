e5554a8c71afaf0ad26c1a161a8b3f52c124d099
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-07-03 17:08:26 +0700

    a

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
-function addDD(a, b) {
-    return a+b;
-}
+const calculator = (function () {
+    function add(a, b) {
+        return a+b;
+    }
 
-function multiply(a,b) {
-    return a*b;
-}
+    function multiply(a,b) {
+        return a*b;
+    }
+    return {
+        add: add,
+        multiply: multiply
+    }
+}) ();
