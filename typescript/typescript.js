cd25ef9bd1377ea070df1097f4db33af1d585b13
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-11-24 16:54:39 +0700

    a

--- typescript/typescript.js
+++ typescript/typescript.js
@@ -11,3 +11,100 @@ var showProductA = function (name, price) {
     console.log("The product ".concat(name, " costs $").concat(price));
 };
 showProductA(productA.name, productA.price);
+// JS is dynamically typed, providing flexibility but also leading to many problems
+// TS adds an optional type system to JS to solve these problems
+// - Type is a label that describes the different properties and methos that a value has
+// Primitive type: string, number, boolean, null, undefined, symbol
+// Object type: function, array, class
+// Show type
+// const headingA = document.querySelector('h1');
+// Type annotation
+var counterA;
+counterA = 1;
+var counterB = 1;
+var nameA = 'John';
+var ageA = 25;
+var activeA = true;
+// Array annotation
+var namesA = ['a', 'b', 'c', 'd'];
+// Object annotation
+var personA;
+personA = {
+    name: 'JOhn',
+    age: 25
+};
+// Function arguments and return types
+var greetingA;
+greetingA = function (name) {
+    return "Hi ".concat(name);
+};
+greetingA = function (name) {
+    return name;
+};
+// Inference type: base on declare to imply type
+// The best common algorithm: analyze item to get best compatible
+// Contextual typing
+// document.addEventListener('click', function (event) {
+//     console.log(event.button)
+// })
+//
+// document.addEventListener('scroll', function (event) {
+//     // console.log(event.button)
+// })
+// Type inference and Type annotation
+// inference guess type, annotation specify explicitly type
+// In practive, always use inference much as impossible
+// Typescript number
+var priceC;
+var priceD = 9.95;
+var counterC = 0;
+var xA = 100, yA = 200;
+var binA = 4;
+var anotherBinA = 2;
+var octalA = 8;
+var hexadecimalA = 0XA;
+// let bigIntA: bigint = 9007199254740991n;
+// Avoid using the Number type as much as possible
+// Typescript String
+// in TS, all strings has string type, use double quote, single quote and backtick to surround string literals
+var firstNameA = 'John';
+var titleA = 'Web Developer';
+var descriptionA = "This TypeScript string can\nspan multiple\nlines";
+console.log(descriptionA);
+// String interpolation: allow embed variabble into string
+var firstNameB = "John";
+var titleB = "Web Developer";
+var profileB = "I'm ".concat(firstNameB, ".\nI'm a ").concat(titleB, "\n");
+console.log(profileB);
+// TypeScript Boolean: has two value is true or false, use boolean keyword to declare boolean variable, don't use Boolean type as much possible
+var pendingA;
+pendingA = true;
+function changeStatusA(status) {
+    return true;
+}
+// Typescript Object Type
+// Primitive type: number, bigint, string, boolean, null, undefined, symbol
+var employeeA;
+employeeA = {
+    firstName: 'John',
+    lsatName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+console.log(employeeA);
+// console.log(employeeA.firstName); //error with TS, JS not error and return undefined
+var employeeB;
+employeeB = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+console.log(employeeB.firstName);
+var employeeC = {
+    firstName: 'Manhnv',
+    lastName: 'Viet',
+    age: 33,
+    jobTitle: 'web'
+};
+console.log(employeeC);
--- typescript/typescript.ts
+++ typescript/typescript.ts
@@ -30,7 +30,7 @@ showProductA(productA.name, productA.price);
 // Primitive type: string, number, boolean, null, undefined, symbol
 // Object type: function, array, class
 // Show type
-const headingA = document.querySelector('h1');
+// const headingA = document.querySelector('h1');
 
 
 // Type annotation
@@ -70,13 +70,13 @@ greetingA = function(name) {
 // Inference type: base on declare to imply type
 // The best common algorithm: analyze item to get best compatible
 // Contextual typing
-document.addEventListener('click', function (event) {
-    console.log(event.button)
-})
-
-document.addEventListener('scroll', function (event) {
-    console.log(event.button)
-})
+// document.addEventListener('click', function (event) {
+//     console.log(event.button)
+// })
+//
+// document.addEventListener('scroll', function (event) {
+//     // console.log(event.button)
+// })
 
 // Type inference and Type annotation
 // inference guess type, annotation specify explicitly type
@@ -85,7 +85,7 @@ document.addEventListener('scroll', function (event) {
 // Typescript number
 let priceC: number;
 let priceD = 9.95;
-let counterB: number = 0;
+let counterC: number = 0;
 let xA: number = 100, yA: number = 200;
 
 let binA = 0b100;
@@ -95,8 +95,71 @@ let octalA: number = 0o10;
 
  let hexadecimalA: number = 0XA;
 
- let bigIntA: bigint = 9007199254740991n;
+ // let bigIntA: bigint = 9007199254740991n;
 // Avoid using the Number type as much as possible
 
 // Typescript String
+// in TS, all strings has string type, use double quote, single quote and backtick to surround string literals
+let firstNameA: string = 'John';
+let titleA: string = 'Web Developer';
+let descriptionA = `This TypeScript string can
+span multiple
+lines`;
+console.log(descriptionA)
+// String interpolation: allow embed variabble into string
+let firstNameB: string = `John`;
+let titleB: string = `Web Developer`;
+let profileB: string = `I'm ${firstNameB}.
+I'm a ${titleB}
+`;
+console.log(profileB)
 
+// TypeScript Boolean: has two value is true or false, use boolean keyword to declare boolean variable, don't use Boolean type as much possible
+let pendingA: boolean;
+pendingA = true;
+
+
+function changeStatusA(status: boolean): boolean {
+    return true;
+}
+
+// Typescript Object Type
+// Primitive type: number, bigint, string, boolean, null, undefined, symbol
+let employeeA: object;
+employeeA = {
+    firstName: 'John',
+    lsatName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+}
+console.log(employeeA)
+// console.log(employeeA.firstName); //error with TS, JS not error and return undefined
+
+let employeeB: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+}
+
+employeeB = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+}
+console.log(employeeB.firstName)
+
+// Combine declare type and assign value
+let employeeC: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+} = {
+    firstName: 'Manhnv',
+    lastName: 'Viet',
+    age: 33,
+    jobTitle: 'web'
+}
+console.log(employeeC)
