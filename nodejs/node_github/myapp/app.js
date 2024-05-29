5238190807f1b5ce6ae78e9e92fa0a46a834f95c
Kevin Pham <kevin.pham1@vmogroup.com>
2024-05-29 15:24:52 +0700

    a

--- nodejs/node_github/myapp/app.js
+++ nodejs/node_github/myapp/app.js
@@ -1,49 +1,116 @@
+// // // // // const express = require('express');
+// // // // // const app = express();
+// // // // //
+// // // // // app.get('/', function(req, res) {
+// // // // //     res.send('Hello World');
+// // // // // })
+// // // // //
+// // // // // app.listen(3000, function () {
+// // // // //     console.log('Example app listening on port 30001!');
+// // // // // })
+// // // //
+// // // //
+// // // // const express = require('express');
+// // // // const app = express();
+// // // //
+// // // // app.get('/', (req, res) => {
+// // // //     console.log('Data send by the client: ', req.query.id);
+// // // //     res.send("<h1>This is our main Page</h1>");
+// // // // })
+// // // //
+// // // // app.get("/about", (req, res) => {
+// // // //     res.send(`
+// // // //     <input type="text" placeholder="Enter your name" />
+// // // //     <button>Submit</button>
+// // // //     `)
+// // // // })
+// // // //
+// // // // // app.get('/data', (req, res) => {
+// // // // //     res.send({
+// // // // //         name: "Manhnv",
+// // // // //         age: 16,
+// // // // //         country: "India"
+// // // // //     })
+// // // // // })
+// // // //
+// // // // app.get('/data', (req, res) => {
+// // // //     res.send([
+// // // //         {
+// // // //             name: "manhnv 1",
+// // // //             age: 16,
+// // // //             country: "india"
+// // // //         },
+// // // //         {
+// // // //             name: "Raj sharma",
+// // // //             age: 17,
+// // // //             country: "India"
+// // // //         }
+// // // //     ])
+// // // // })
+// // // //
+// // // // app.listen(5001);
+// // //
+// // //
 // // // const express = require('express');
 // // // const app = express();
 // // //
-// // // app.get('/', function(req, res) {
-// // //     res.send('Hello World');
+// // // app.get('/', (req, res) => {
+// // //     console.log("Data send by the client: ", req.query.id);
+// // //     res.send("<h1>This is our main page</h1> <a href='/about'>About</a>")
 // // // })
 // // //
-// // // app.listen(3000, function () {
-// // //     console.log('Example app listening on port 30001!');
+// // // app.get('/about', (req, res) => {
+// // //     res.send(`
+// // //     <input type="text" placeholder="Enter your name" />
+// // //     <button>Submit</button>
+// // //     <a href="/">Home</a>
+// // //     `)
 // // // })
+// // //
+// // // app.get('/data', (req, res) => {
+// // //     res.send([
+// // //         {
+// // //             name: 'manhvn1',
+// // //             age: 16,
+// // //             country: "vn1"
+// // //         },
+// // //         {
+// // //             name: 'manhnv2',
+// // //             age: 18,
+// // //             country: "vn2"
+// // //         }
+// // //     ])
+// // // })
+// // //
+// // // app.listen(5001)
 // //
 // //
 // // const express = require('express');
 // // const app = express();
 // //
 // // app.get('/', (req, res) => {
-// //     console.log('Data send by the client: ', req.query.id);
-// //     res.send("<h1>This is our main Page</h1>");
+// //     console.log("Data send by the client: ", req.query.id);
+// //     res.send("<h1>This is our main page</h1> <a href='/about'>About</a>");
 // // })
 // //
-// // app.get("/about", (req, res) => {
+// // app.get('/about', (req, res) => {
 // //     res.send(`
-// //     <input type="text" placeholder="Enter your name" />
-// //     <button>Submit</button>
+// //     <input type="text" placeholder="Enter your name" value=${req.query.name} />
+// //     <a href="/">Home</a>
 // //     `)
 // // })
-// //
-// // // app.get('/data', (req, res) => {
-// // //     res.send({
-// // //         name: "Manhnv",
-// // //         age: 16,
-// // //         country: "India"
-// // //     })
-// // // })
 // //
 // // app.get('/data', (req, res) => {
 // //     res.send([
 // //         {
-// //             name: "manhnv 1",
+// //             name: "manhnv1",
 // //             age: 16,
 // //             country: "india"
 // //         },
 // //         {
-// //             name: "Raj sharma",
+// //             name: "manhnv2",
 // //             age: 17,
-// //             country: "India"
+// //             country: "india2"
 // //         }
 // //     ])
 // // })
@@ -52,67 +119,33 @@
 //
 //
 // const express = require('express');
+// const path = require('path');
+//
 // const app = express();
+// const publicPath = path.join(__dirname, 'public');
 //
-// app.get('/', (req, res) => {
-//     console.log("Data send by the client: ", req.query.id);
-//     res.send("<h1>This is our main page</h1> <a href='/about'>About</a>")
-// })
-//
-// app.get('/about', (req, res) => {
-//     res.send(`
-//     <input type="text" placeholder="Enter your name" />
-//     <button>Submit</button>
-//     <a href="/">Home</a>
-//     `)
-// })
-//
-// app.get('/data', (req, res) => {
-//     res.send([
-//         {
-//             name: 'manhvn1',
-//             age: 16,
-//             country: "vn1"
-//         },
-//         {
-//             name: 'manhnv2',
-//             age: 18,
-//             country: "vn2"
-//         }
-//     ])
+// app.get("/about", (req, res) => {
+//     res.sendFile(`${publicPath}/about.html`)
 // })
 //
 // app.listen(5001)
 
-
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const publicPath = path.join(__dirname, 'public');
 
 app.get('/', (req, res) => {
-    console.log("Data send by the client: ", req.query.id);
-    res.send("<h1>This is our main page</h1> <a href='/about'>About</a>");
+    res.sendFile(`${publicPath}/about.html`);
 })
 
 app.get('/about', (req, res) => {
-    res.send(`
-    <input type="text" placeholder="Enter your name" value=${req.query.name} />
-    <a href="/">Home</a>
-    `)
+    res.sendFile(`${publicPath}/about.html`);
 })
 
-app.get('/data', (req, res) => {
-    res.send([
-        {
-            name: "manhnv1",
-            age: 16,
-            country: "india"
-        },
-        {
-            name: "manhnv2",
-            age: 17,
-            country: "india2"
-        }
-    ])
+app.get("*", (req, res) => {
+    res.sendFile(`${publicPath}/404.html`);
 })
 
-app.listen(5001);
+app.listen(5001)
